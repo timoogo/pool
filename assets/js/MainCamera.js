407bca8f1a28f7b6f551443788e30855794bd749
Timothée Gaultier <tiimogo@gmail.com>
2021-12-11 16:38:21 +0100

    b4 gltf

--- assets/js/MainCamera.js
+++ assets/js/MainCamera.js
@@ -2,15 +2,57 @@ import style from '@/css/style.css';
 import {DebuggerGUI} from './GUI.js'	
 
 import * as THREE from 'three'
-import {Ball} from "./models/Ball.js"
-import {Table} from "./models/Table.js"
+import {Ball} from "../../models/Ball.js"
+// import {Table} from "./models/Table.js"
 
 
+import { OBJLoader } from './jsm/loaders/OBJLoader.js';
+import { MTLLoader } from './jsm/loaders/MTLoader.js'
+
+let table
+function loadModel() {
+
+	table.traverse( function ( child ) {
+
+		if ( child.isMesh ) child.material.map = texture;
+
+	} );
+
+	table.position.y = - 95;
+	scene.add( table );
+
+}
+
+const manager = new THREE.LoadingManager( loadModel );
+
+manager.onProgress = function ( item, loaded, total ) {
+
+	console.log( item, loaded, total );
+
+};
+
+// texture
+
+const mtlLoader = new MTLLoader( manager );
+const texture = mtlLoader.load( 'models/obj/Pool.mtl' );
 
 
+function onProgress( xhr ) {
 
+	if ( xhr.lengthComputable ) {
 
+		const percentComplete = xhr.loaded / xhr.total * 100;
+		console.log( 'model ' + Math.round( percentComplete, 2 ) + '% downloaded' );
 
+	}
+
+}
+function onError() {}
+
+const loader = new OBJLoader( manager );
+loader.load( 'models/obj/Pool.obj', function ( obj ) {
+	table = obj;
+}, onProgress, onError );
 
 const renderer = new THREE.WebGLRenderer();
 const scene = new THREE.Scene();
@@ -21,12 +63,7 @@ const whiteBall = new Ball(new THREE.SphereGeometry(6,30,30), new THREE.MeshLamb
 const redBall = new Ball(new THREE.SphereGeometry(6,30,30), new THREE.MeshLambertMaterial({
 	color: "red"
 }))
-// holetest
-const material2 = new THREE.MeshBasicMaterial({color: 0xffff00 });
-const geometry2 = new THREE.CylinderGeometry(20, 20, 24, 30, 30, false);
-const cylinder = new THREE.Mesh(geometry2, material2);
-// end 
-const table = new Table(new THREE.BoxGeometry(150, 300 ,3), new THREE.MeshLambertMaterial({transparent: true }))
+// const table = new Table(new THREE.BoxGeometry(150, 300 ,3), new THREE.MeshLambertMaterial({transparent: true }))
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight , 0.1, 1000 );
 
 
@@ -40,23 +77,22 @@ function Light() {
 	var object3d = new THREE.DirectionalLight(0x93C54B, 0.55);
 	object3d.position.set(0, 30, 200);
 	object3d.name = 'Back light';
-	scene.add(object3d);
 	object3d = new THREE.DirectionalLight('white', 0.35);
 	object3d.position.set(-6, -3, 0);
 	object3d.name = 'Key light';
-	scene.add(object3d);
 	object3d = new THREE.DirectionalLight('white', 0.55);
 	object3d.position.set(9, 9, 6);
 	object3d.name = 'Fill light';
-	scene.add(object3d);
 	var spotLight = new THREE.SpotLight(0xffffff);
 	spotLight.position.set(3, 30, 3);
 	spotLight.castShadow = true;
 	var backLight = new THREE.SpotLight(0x000000);
 	backLight.position.set(0, 30, -300);
 	backLight.castShadow = true;
-	
-	scene.add(spotLight, backLight);
+	const groupLight = new THREE.Group();
+	groupLight.add(spotLight, backLight, object3d)
+	scene.add(groupLight);
+	groupLight.name = "Lighting"
 }
 
 
@@ -69,24 +105,16 @@ export function init(){
 
 
     const material = new THREE.MeshLambertMaterial({transparent: true });
-	
-	
-	// mesh.position.z = 0 
-	whiteBall.parent = table
-	redBall.position.x= 15
-	redBall.position.z= 10
+
+	redBall.position.y = -86
 	whiteBall.position.z =10 
 	whiteBall.name = 'Boule Blanche'
-	cylinder.position.x = 15
-	cylinder.position.z = 15
-	// console.log(whiteBall.parent)
 	// const axisHelper = new THREE.AxesHelper()
 
 
-	new DebuggerGUI(table,redBall, material, camera, renderer)
-	cylinder.parent = table
-	cylinder.rotation.x = 45
-	scene.add( table, whiteBall, redBall,/* holetest */ cylinder /* end holetest */  );
+	new DebuggerGUI(/* table, */ whiteBall,  redBall, material, camera, renderer)
+	
+	scene.add( /* table,*/ whiteBall, redBall  );
 	// scene.add( axisHelper);
 
 }
@@ -95,9 +123,9 @@ function  rand (min, max) {
 	return Math.random(min, max)
 }
 export function animate() {
-	redBall.MotionDesign(scene, table, redBall)
+	redBall.MotionDesign(scene, /*table,*/  redBall)
 	//console.log(velY)
-	whiteBall.Move(scene, table)
+	whiteBall.Move(scene, /* table */)
 
 
 	requestAnimationFrame( animate );
